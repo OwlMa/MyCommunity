6504782cd2005446ef4df1a24cae1a43a8264e7c
OwlMa <matianyang0603@gmail.com>
2020-04-06 18:01:49 -0400

    add index and other static files like css

--- src/main/java/community/community/Controller/IndexController.java
+++ src/main/java/community/community/Controller/IndexController.java
@@ -6,12 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class IndexController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
-
 }
