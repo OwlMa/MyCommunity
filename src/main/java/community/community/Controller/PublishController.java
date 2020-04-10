9568f77fdf335847a63e6ed37dabacb1f74374be
OwlMa <matianyang0603@gmail.com>
2020-04-09 21:21:39 -0400

    add publish page publish.index

---
+++ src/main/java/community/community/Controller/PublishController.java
@@ -0,0 +1,13 @@
+package community.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublishController {
+
+    @GetMapping("/publish")
+    public String publish() {
+        return "publish";
+    }
+}
