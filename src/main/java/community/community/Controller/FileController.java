4aabdf222d285048282cab3d6a3eee4c717044ee
OwlMa <matianyang0603@gmail.com>
2020-07-25 01:48:15 -0400

    add local image upload

---
+++ src/main/java/community/community/Controller/FileController.java
@@ -0,0 +1,18 @@
+package community.community.Controller;
+
+import community.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload() {
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/img/deer.jpg");
+        return fileDTO;
+    }
+}
